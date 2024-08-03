001610302eb128cf44740d5204618d6da2b43266
paulingalls <paulingalls@gmail.com>
2024-08-02 18:49:52 -0700

    move AI stuff to it's own thread

---
+++ utils/ai_thread.py
@@ -0,0 +1,70 @@
+import threading
+from queue import Queue
+import numpy as np
+from av.audio.frame import AudioFrame
+from av.audio.resampler import AudioResampler
+from models.llm import LLM
+from models.vad import VAD
+from models.whisper import Whisper
+from models.melo import Melo
+
+MODEL_SAMPLE_RATE = 16000
+
+
+class AIThread(threading.Thread):
+    def __init__(self, input_queue: Queue, output_queue: Queue):
+        super().__init__(name='AIThread')
+        self.input_queue = input_queue
+        self.output_queue = output_queue
+        self.daemon = True
+        self.buffer = None
+        self.voice_buffer = None
+        self.count = 1
+        self.resampler = AudioResampler(format="s16", layout="mono", rate=MODEL_SAMPLE_RATE)
+        self.vad = VAD(MODEL_SAMPLE_RATE)
+        self.whisper = Whisper()
+        self.melo = Melo()
+        self.sentence = ""
+
+    def down_sample(self, frame: AudioFrame) -> AudioFrame:
+        new_frames = self.resampler.resample(frame)
+        return new_frames[0]
+
+    def on_text(self, text: str, _):
+        self.sentence += text
+        if text == "." or text == "!" or text == "?" or text.endswith(".\""):
+            sentence_audio = self.melo.generate(self.sentence)
+            self.sentence = ""
+            self.output_queue.put(sentence_audio)
+
+    def start_llm_response(self, buffer):
+        text_from_voice = self.whisper.get_text(buffer)
+        LLM.get_response(text_from_voice, self.on_text)
+
+    def run(self):
+        while True:
+            frame: AudioFrame = self.input_queue.get()
+            resampled = self.down_sample(frame)
+            if resampled.samples < 320:
+                self.input_queue.task_done()
+                continue
+            if self.buffer is None:
+                self.buffer = resampled.to_ndarray()
+            elif self.count < 8:
+                self.count += 1
+                self.buffer = np.concatenate((self.buffer, resampled.to_ndarray()), axis=1)
+            else:
+                voice_data = self.vad.vad(self.buffer)
+                if "start" in voice_data:
+                    if "end" in voice_data:
+                        self.start_llm_response(self.buffer)
+                    else:
+                        self.voice_buffer = self.buffer
+                elif "end" in voice_data:
+                    self.start_llm_response(self.voice_buffer)
+                    self.voice_buffer = None
+                elif self.voice_buffer is not None:
+                    self.voice_buffer = np.concatenate((self.voice_buffer, self.buffer), axis=1)
+                self.buffer = resampled.to_ndarray()
+                self.count = 1
+            self.input_queue.task_done()
--- utils/transform.py
+++ utils/transform.py
@@ -1,16 +1,12 @@
+from queue import Queue
 from aiortc import MediaStreamTrack
 from av.audio.frame import AudioFrame
-from av.audio.resampler import AudioResampler
 from av.utils import Fraction
 from pyee.asyncio import AsyncIOEventEmitter
 import numpy as np
 
-from models.llm import LLM
-from models.vad import VAD
-from models.whisper import Whisper
-from models.melo import Melo
+from utils.ai_thread import AIThread
 
-MODEL_SAMPLE_RATE = 16000
 OUTPUT_SAMPLE_RATE = 44100
 
 
@@ -20,53 +16,34 @@ class Transform(MediaStreamTrack, AsyncIOEventEmitter):
     def __init__(self, track):
         super().__init__()
         self.track = track
-        self.vad = None
-        self.buffer = None
-        self.voice_buffer = None
-        self.count = 1
-        self.resampler = AudioResampler(format="s16", layout="mono", rate=MODEL_SAMPLE_RATE)
-        self.vad = VAD(MODEL_SAMPLE_RATE)
-        self.whisper = Whisper()
-        self.melo = Melo()
-        self.sentence = ""
         self.response_buffer = None
-
-        @self.vad.on("voiceStart")
-        async def on_voice_start(data):
-            self.emit("voiceStart", data)
-
-        @self.vad.on("voiceEnd")
-        async def on_voice_start(data):
-            self.emit("voiceEnd", data)
-
-    def down_sample(self, frame: AudioFrame) -> AudioFrame:
-        new_frames = self.resampler.resample(frame)
-        return new_frames[0]
-
-    def on_text(self, text: str, _):
-        self.sentence += text
-        if text == "." or text == "!" or text == "?":
-            self.emit("response", f"sentence: {self.sentence}")
-            sentence_audio = self.melo.generate(self.sentence)
-            if self.response_buffer is None:
-                self.response_buffer = sentence_audio
-            else:
-                self.response_buffer = np.concatenate((self.response_buffer, sentence_audio), axis=0)
-            self.sentence = ""
+        self.write_queue = Queue(0)
+        self.read_queue = Queue(0)
+        self.ai_thread = AIThread(self.write_queue, self.read_queue)
+        self.ai_thread.start()
 
     @staticmethod
-    def get_silent_frame(samples, pts) -> AudioFrame:
+    def get_silent_frame(samples, sample_rate, pts) -> AudioFrame:
         silence = np.zeros((1, samples), dtype='int16')
         new_frame = AudioFrame.from_ndarray(silence, 's16', layout="mono")
         new_frame.sample_rate = OUTPUT_SAMPLE_RATE
         new_frame.time_base = Fraction(1, OUTPUT_SAMPLE_RATE)
-        new_frame.pts = round(pts * OUTPUT_SAMPLE_RATE / MODEL_SAMPLE_RATE)
+        new_frame.pts = round(pts * OUTPUT_SAMPLE_RATE / sample_rate)
         return new_frame
 
-    def get_next_frame(self, samples, pts) -> AudioFrame:
+    def pull_audio_from_queue(self):
+        while self.read_queue.qsize() > 0:
+            audio_data = self.read_queue.get()
+            if self.response_buffer is None:
+                self.response_buffer = audio_data
+            else:
+                self.response_buffer = np.concatenate((self.response_buffer, audio_data), axis=0)
+            self.read_queue.task_done()
+
+    def get_next_frame(self, samples, sample_rate, pts) -> AudioFrame:
         next_samples = None
         if self.response_buffer is None:
-            return self.get_silent_frame(samples, pts)
+            return self.get_silent_frame(samples, sample_rate, pts)
         elif len(self.response_buffer) < samples:
             next_samples = self.response_buffer * 32768
             next_samples = np.concatenate((next_samples,
@@ -80,41 +57,12 @@ class Transform(MediaStreamTrack, AsyncIOEventEmitter):
         next_frame = AudioFrame.from_ndarray(np.array([np.asarray(next_samples, dtype=np.int16)]), 's16', layout="mono")
         next_frame.sample_rate = OUTPUT_SAMPLE_RATE
         next_frame.time_base = Fraction(1, OUTPUT_SAMPLE_RATE)
-        next_frame.pts = round(pts * OUTPUT_SAMPLE_RATE / MODEL_SAMPLE_RATE)
+        next_frame.pts = round(pts * OUTPUT_SAMPLE_RATE / sample_rate)
         return next_frame
 
     async def recv(self):
         frame: AudioFrame = await self.track.recv()
-        resampled = self.down_sample(frame)
-        if resampled.samples < 320:
-            output_samples = round(resampled.samples * OUTPUT_SAMPLE_RATE / MODEL_SAMPLE_RATE)
-            return self.get_silent_frame(output_samples, resampled.pts)
-
-        if self.buffer is None:
-            self.buffer = resampled.to_ndarray()
-        elif self.count < 8:
-            self.count += 1
-            self.buffer = np.concatenate((self.buffer, resampled.to_ndarray()), axis=1)
-        else:
-            voice_data = self.vad.vad(self.buffer)
-            if "start" in voice_data:
-                if "end" in voice_data:
-                    text_from_voice = self.whisper.get_text(self.buffer)
-                    self.emit("text", text_from_voice)
-                    response = LLM.get_response(text_from_voice, self.on_text)
-                    self.emit("response", response)
-                else:
-                    self.voice_buffer = self.buffer
-            elif "end" in voice_data:
-                text_from_voice = self.whisper.get_text(self.voice_buffer)
-                self.voice_buffer = None
-                self.emit("text", text_from_voice)
-                response = LLM.get_response(text_from_voice, self.on_text)
-                self.emit("response", response)
-            elif self.voice_buffer is not None:
-                self.voice_buffer = np.concatenate((self.voice_buffer, self.buffer), axis=1)
-            self.buffer = resampled.to_ndarray()
-            self.count = 1
-
-        output_samples = round(resampled.samples * OUTPUT_SAMPLE_RATE / MODEL_SAMPLE_RATE)
-        return self.get_next_frame(output_samples, resampled.pts)
+        self.write_queue.put_nowait(frame)
+        self.pull_audio_from_queue()
+        output_samples = round(frame.samples * OUTPUT_SAMPLE_RATE / frame.sample_rate)
+        return self.get_next_frame(output_samples, frame.sample_rate, frame.pts)
